2c65bf46f94ba41c9dad62e2b432b1fb50ce75f2
jasnt05 <166681726+jasnt05@users.noreply.github.com>
2024-04-18 01:57:28 -0700

    Update index.js

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ client.once('ready', () => {
   setTimeout(() => {
     console.log('\x1b[32m%s\x1b[0m', `|    🎯 Activity sucessfully set!`);
     client.user.setPresence({
-      activities: [{ name: `Update Out V@1.5`, type: ActivityType.Watching }],
+      activities: [{ name: `Luan kiss Henry`, type: ActivityType.Watching }],
       status: 'idle',
     });
   }, 2000); 
