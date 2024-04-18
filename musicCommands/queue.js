cbb44c711cc645ba293675b33e181d5d1a8e652f
jasnt05 <166681726+jasnt05@users.noreply.github.com>
2024-04-17 22:23:32 -0700

    Update queue.js

--- musicCommands/queue.js
+++ musicCommands/queue.js
@@ -10,10 +10,9 @@ module.exports = {
       const embed = new EmbedBuilder()
       .setAuthor({
           name: 'Attention',
-          iconURL: 'https://cdn.discordapp.com/attachments/1223544847047065662/1224631171766292500/9596-wrong.gif?ex=661e31a7&is=660bbca7&hm=0176645a3d582d6b93c8447a02cd7b1e7923b316212336fdc0b23b96b5e8ab4b&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://cdn.discordapp.com/attachments/821165280612319262/1230291345382314065/ryan.png?ex=6632c91a&is=6620541a&hm=deb2ac5108359a5878c6172c5dbab79171aabe75d3fd5a998069219068eaae57&',
         })
-      .setDescription('**The Queue is currently empty consider adding songs.**')
+      .setDescription('**The Queue is currently empty**')
       .setColor('#ff0000');
     return message.reply({ embeds: [embed] });
     }
@@ -22,8 +21,7 @@ module.exports = {
       .setColor('#2b71ec')
      .setAuthor({
           name: 'Queue',
-          iconURL: 'https://cdn.discordapp.com/attachments/1175488636033175602/1175488721001398333/queue.png?ex=656b6a2e&is=6558f52e&hm=7573613cbb8dcac83ba5d5fc55ca607cf535dd117b4492b1c918d619aa6fd7ad&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          iconURL: 'https://cdn.discordapp.com/attachments/821165280612319262/1230291345382314065/ryan.png?ex=6632c91a&is=6620541a&hm=deb2ac5108359a5878c6172c5dbab79171aabe75d3fd5a998069219068eaae57&',
         })
       .setDescription(queue.map((song, index) => `**${index + 1}.** ${song.searchQuery}`).join('\n'));
 
